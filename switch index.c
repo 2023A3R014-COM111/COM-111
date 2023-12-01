221f78dc123cc48368dcad043feed883662dc689
2023A3R014-COM111 <146520678+2023A3R014-COM111@users.noreply.github.com>
2023-12-01 11:08:19 +0530

    Add files via upload

---
+++ switch index.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int marks;
+    int grade;
+
+    printf("enter the student's marks : ");
+    scanf("%d",&marks);
+
+    int index = marks / 10;
+
+    switch (index)
+    {
+        case 10:
+        case 9:
+        case 8:
+            grade = 'H';
+            break;
+        case 7:
+        case 6:
+            grade = '1';
+            break;
+        case 5:
+            grade = '2';
+            break;
+        case 4:
+            grade = '3';
+            break;
+        default:
+            grade = 'F';
+            break;
+    }
+    printf("the  grade obtained by the student is : %c\n",grade);
+    return 0;
+}
+
+
+
